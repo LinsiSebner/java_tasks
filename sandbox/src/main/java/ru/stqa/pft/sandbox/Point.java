b9352be1af7e42daf5de01f70ee33a6769bac4fb
LinsiSebner <linsisebner@gmail.com>
2022-05-02 14:22:46 +0300

    Задание 2
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
    
    2. Создать функцию
    public static double distance(Point p1, Point p2)
    которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
    
    3. Сделать запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {...}
    и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
    
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.
    
    ---
    
    После выполнения задания сохраните код в удаленный репозиторий и в качестве ответа на задание сообщите тренеру ссылку на последний коммит, а также можете написать любые дополнительные комментарии и вопросы.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public static void main(String[] args) {
+    PointDatas d = new PointDatas(1,5, 6,7);
+    System.out.println("Дистанция= " + distance(d));
+  }
+
+  public static double distance(PointDatas d) {
+    return Math.sqrt(d.x2 - d.x1) * (d.x2 - d.x1) + (d.y2 - d.y1) * (d.y2 - d.y1);
+  }
+}
